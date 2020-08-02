f62b5247b9cb1c503dcc073bda74bd00644722c7
l1ghtman2k <aibek.zhil@gmail.com>
2020-08-02 00:30:49 -0400

    Add Index parameter which allows for shorthand X substitution

--- pkg/team/team.go
+++ pkg/team/team.go
@@ -1,6 +1,7 @@
 package team
 
 import (
+	"errors"
 	"github.com/ScoreTrak/Web/pkg/user"
 	"github.com/gofrs/uuid"
 	"gorm.io/gorm"
@@ -9,20 +10,25 @@ import (
 type Team struct {
 	ID uuid.UUID `json:"id,omitempty" gorm:"type:uuid;primary_key;"`
 
-	Name string `json:"name" gorm:"unique;not null;default:null" valid:"required,alphanum"`
+	Name string `json:"name" gorm:"unique;not null" valid:"required,alphanum"`
+
+	Index *uint `json:"index" gorm:"unique;not null"`
 
 	Users []*user.User `gorm:"foreignkey:TeamID;association_foreignkey:ID" json:"-"`
 
 	Enabled *bool `json:"enabled,omitempty" gorm:"-"`
 }
 
-func (p *Team) BeforeCreate(tx *gorm.DB) (err error) {
-	if p.ID == uuid.Nil {
+func (t *Team) BeforeCreate(tx *gorm.DB) (err error) {
+	if t.Name == "" {
+		return errors.New("field Name is a mandatory parameter")
+	}
+	if t.ID == uuid.Nil {
 		u, err := uuid.NewV4()
 		if err != nil {
 			return err
 		}
-		p.ID = u
+		t.ID = u
 	}
 	return nil
 }
