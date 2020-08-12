0000063b4db455effb4048d4706e7a5cd4f29f09
l1ghtman2k <aibek.zhil@gmail.com>
2020-08-11 22:18:20 -0400

    Renive bad "enabled" column

--- cmd/jobs/main.go
+++ cmd/jobs/main.go
@@ -39,10 +39,10 @@ func main() {
 	}
 
 	if *preloadData {
-		db.Exec("INSERT INTO web_teams (id, name, enabled, team_index) VALUES ('11111111-1111-1111-1111-111111111111', 'TeamOne', 1)")
-		db.Exec("INSERT INTO web_teams (id, name, enabled, team_index) VALUES ('22222222-2222-2222-2222-222222222222', 'TeamTwo', 2)")
-		db.Exec("INSERT INTO web_teams (id, name, enabled, team_index) VALUES ('33333333-3333-3333-3333-333333333333', 'TeamThree', 3)")
-		db.Exec("INSERT INTO web_teams (id, name, enabled, team_index) VALUES ('44444444-4444-4444-4444-444444444444', 'TeamFour', 4)")
+		db.Exec("INSERT INTO web_teams (id, name, team_index) VALUES ('11111111-1111-1111-1111-111111111111', 'TeamOne', 1)")
+		db.Exec("INSERT INTO web_teams (id, name, team_index) VALUES ('22222222-2222-2222-2222-222222222222', 'TeamTwo', 2)")
+		db.Exec("INSERT INTO web_teams (id, name, team_index) VALUES ('33333333-3333-3333-3333-333333333333', 'TeamThree', 3)")
+		db.Exec("INSERT INTO web_teams (id, name, team_index) VALUES ('44444444-4444-4444-4444-444444444444', 'TeamFour', 4)")
 	}
 
 }
